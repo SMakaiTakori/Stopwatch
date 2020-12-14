4d3feea3df733af4b8c1cde0de694e70d519a9e5
SMakaiTakori <makait87@icloud.com>
2020-12-14 13:46:37 -0500

    created start and stop functions

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Stopwatch
+This is a small exercise/project to create a stopwatch using JS
---
+++ index.js
@@ -0,0 +1,30 @@
+function Stopwatch(){
+    let startTime, endTime, running, duration = 0;
+
+    this.start() = function(){
+
+        if(running)
+            throw new Error('Stopwatch has already started.');
+
+        running = true;
+
+        startTime = new Date();
+
+    };
+
+    this.stop() = function(){
+        if(!running)
+            throw new Error('Stopwatch has stopped.');
+
+        running = false;
+        endTime = new Date();  
+              
+        const seconds = (endTime.getTime() - startTime.getTime() / 1000);
+        duration += seconds;
+    };
+
+    this.reset() = function(){
+        
+    };
+
+}
